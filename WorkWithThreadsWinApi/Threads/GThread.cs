4e71e0f57102fc02e51a08bbde29255f32dc3260
SpaceDandy406 <rocky.j59@yandex.ru>
2017-06-27 14:57:55 +0500

    делегат теперь хранится в переменной класса

--- WorkWithThreadsWinApi/Threads/GThread.cs
+++ WorkWithThreadsWinApi/Threads/GThread.cs
@@ -1,65 +1,65 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace WorkWithThreadsWinApi.Threads
 {
-    public class GThread
-    {
-        private bool flag = true;
+	public class GThread
+	{
+		private bool _flag = true;
+		private readonly ThreadDelegate _delegate;
 
-        public ThreadPriority Priority
-        {
-            get { return WinApiHelper.GetThreadPriority(Handle); }
-            set { WinApiHelper.SetThreadPriority(Handle, (int)value); }
-        }
+		public ThreadPriority Priority
+		{
+			get { return WinApiHelper.GetThreadPriority(Handle); }
+			set { WinApiHelper.SetThreadPriority(Handle, (int)value); }
+		}
 
-        public uint Id { get; private set; }
+		public uint Id { get; private set; }
 
-        public uint Handle { get; private set; }
+		public uint Handle { get; private set; }
 
-        public TimeSpan ThreadTime
-        {
-            get { return DiagnosticHelper.GetThreadTime(Id); }
-        }
+		public TimeSpan ThreadTime
+		{
+			get { return DiagnosticHelper.GetThreadTime(Id); }
+		}
 
-        public GThread()
-        {
-        }
+		public GThread()
+		{
+			_delegate += CalculatePi;
+		}
 
-        public unsafe void Start()
-        {
-            var threadDelegate = new ThreadDelegate(CalculatePi);
-            var func = Marshal.GetFunctionPointerForDelegate(threadDelegate);
+		public void Start()
+		{
+			var func = Marshal.GetFunctionPointerForDelegate(_delegate);
 
-            uint lpThreadId;
-            Handle = WinApiHelper.CreateThread(IntPtr.Zero, 0, func, IntPtr.Zero, 0, out lpThreadId);
-            Id = lpThreadId;
-        }
+			uint lpThreadId;
+			Handle = WinApiHelper.CreateThread(IntPtr.Zero, 0, func, IntPtr.Zero, 0, out lpThreadId);
+			Id = lpThreadId;
+		}
 
-        public GThread(ThreadPriority priority)
-            : this()
-        {
-            Priority = priority;
-        }
+		public GThread(ThreadPriority priority)
+			: this()
+		{
+			Priority = priority;
+		}
 
-        public void DestroyGThread()
-        {
-            flag = false;
-            WinApiHelper.WaitForSingleObject(Handle, 10000);
-            WinApiHelper.CloseHandle(Handle);
-        }
+		public void DestroyGThread()
+		{
+			_flag = false;
+			WinApiHelper.WaitForSingleObject(Handle, 10000);
+			WinApiHelper.CloseHandle(Handle);
+		}
 
-        private void CalculatePi()
-        {
-            double i = 0;
-            double pi = 0;
-            while (flag)
-            {
-                pi += (1.0 / (1.0 + 2.0 * i)) * ((i % 2 == 0) ? 1 : (-1));
-                i++;
-            }
-        }
-    }
+		private void CalculatePi()
+		{
+			double i = 0;
+			// ReSharper disable once NotAccessedVariable
+			double pi = 0;
+			while (_flag)
+			{
+				pi += (1.0 / (1.0 + 2.0 * i)) * ((i % 2 == 0) ? 1 : (-1));
+				i++;
+			}
+		}
+	}
 }
